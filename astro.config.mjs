58c7b0b09d9ce848c6dd4bfa3e0d35550f56b2c5
RicardoCR98 <gary.campana@epn.edu.ec>
2024-02-04 11:52:21 -0500

    Cambiando el URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'https://ricardocr98.github.io',
+  site: {
+    baseUrl: '/Portfolio',
+  },
 });
