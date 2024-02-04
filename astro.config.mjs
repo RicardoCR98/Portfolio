43b706831d27f5054466e94c30c1155193d11e7e
RicardoCR98 <gary.campana@epn.edu.ec>
2024-02-04 02:24:49 -0500

    Modificacion del .yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://ricardocr98.github.io',
+    site: 'https://ricardocr98.github.io/Portfolio/',
 });
